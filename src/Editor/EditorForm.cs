131545fd7958076105028ee59b6a011d57ef6dc2
Joseph Plant <git@JosephPlant.com>
2013-06-17 15:49:32 +1000

    Add an Empty Editor project

---
+++ src/Editor/EditorForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace AmplaTools.ProjectCreate.Editor
+{
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
